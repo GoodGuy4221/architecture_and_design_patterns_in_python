220ccec29f4d4929b8221a7590838a282fcd95ff
Eric <gizyatullov0@gmail.com>
2022-09-04 16:21:04 +0400

    '.'

---
+++ run.py
@@ -0,0 +1,10 @@
+from wsgiref.simple_server import make_server
+
+from rast_framework.main import Rust
+from urls import routes, fronts
+
+app = Rust(routes, fronts)
+
+with make_server('', 5195, app) as httpd:
+    print(f'Запуск на 127.0.0.1:{httpd.server_port}')
+    httpd.serve_forever()
